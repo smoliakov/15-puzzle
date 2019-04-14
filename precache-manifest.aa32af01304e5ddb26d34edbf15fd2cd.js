04a84a8ddc3b046dd0759fa83814b2fbf3fbf45a
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-04-14 11:37:44 +0000

    Deploy smoliakov/15-puzzle to github.com/smoliakov/15-puzzle.git:gh-pages

---
+++ precache-manifest.aa32af01304e5ddb26d34edbf15fd2cd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "bae4684018ccced23f88",
+    "url": "/15-puzzle/static/js/runtime~main.8815c511.js"
+  },
+  {
+    "revision": "833715dc691a6052b107",
+    "url": "/15-puzzle/static/js/main.95726448.chunk.js"
+  },
+  {
+    "revision": "b4a520cc173ed3482b6d",
+    "url": "/15-puzzle/static/js/2.808575a5.chunk.js"
+  },
+  {
+    "revision": "833715dc691a6052b107",
+    "url": "/15-puzzle/static/css/main.2ff1ed23.chunk.css"
+  },
+  {
+    "revision": "0334104898ba6bac3ea6f53aff7f28ad",
+    "url": "/15-puzzle/index.html"
+  }
+];
